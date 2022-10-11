2ca4be2d7e875dc0808116fc0ce28ee02071e329
jgioegana <jgioegana@gmail.com>
2022-10-11 12:31:18 -0700

    Fixed minor cloud animation issue

--- libraries/script.js
+++ libraries/script.js
@@ -5,6 +5,7 @@ gsap.to('#sec-1-wave',{yPercent: -2, yoyo: true, repeat: -1, ease: 'power1.inOut
 gsap.fromTo('#cloud1',{yPercent: 20}, {yPercent: 0, yoyo: true, repeat: -1, ease: 'power1.inOut', duration: 1.5});
 gsap.fromTo('#cloud2',{yPercent: 0}, {yPercent: 20, yoyo: true, repeat: -1, ease: 'power1.inOut', duration: 1});
 gsap.fromTo('#cloud3',{yPercent: 20}, {yPercent: 0, yoyo: true, repeat: -1, ease: 'power1.inOut', duration: 1.5});
+gsap.fromTo('#cloud4',{yPercent: 0}, {yPercent: 20, yoyo: true, repeat: -1, ease: 'power1.inOut', duration: 1});
 
 function showCollapse(item){
     var faqAns = document.getElementById('sec-faq-ans-cont' + '_' + item).getBoundingClientRect();
